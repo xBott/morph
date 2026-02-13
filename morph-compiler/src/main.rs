c4b1505a92ed6d0274cd902d0acbcfbcccfe6bef
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-13 02:06:15 +0100

    main file

--- morph-compiler/src/main.rs
+++ morph-compiler/src/main.rs
@@ -1,42 +1,57 @@
-mod file;
+mod utils;
 mod lexer;
 mod token;
 mod parser;
-
+mod generator;
+mod generators;
+use crate::generator::{create_generator, Generator, Lang};
+use crate::lexer::{Lexer, SimpleLexer};
+use crate::parser::{Parser, SimpleParser};
 use std::env;
 use std::fs::File;
-use std::path::{PathBuf};
-use crate::file::FileWrapper;
-use crate::lexer::{Lexer, SimpleLexer};
-use crate::token::Token;
+use std::path::PathBuf;
+use crate::utils::FileWrapper;
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
-    let path: PathBuf = env::args().nth(1).expect("Usage: ...").into();
-    println!("Compiling {}", path.to_string_lossy());
+    let mut args = env::args().skip(1);
 
-    match File::open(&path) {
-        Ok(file) => {
-            let mut wrapper = FileWrapper { path, file };
-            let lexer = SimpleLexer;
-            match lexer.tokenize(&mut wrapper) {
-                Ok(tokens) => {
-                    print_tokens(&tokens);
-                },
-                Err(e) => {
-                    eprintln!("{}", e);
-                }
-            };
-        }
-        Err(e) => {
-            eprintln!("{}", e);
-        }
+    let lang: Lang = args
+        .next()
+        .expect("Usage: morph <lang> <file> [output_dir]")
+        .parse()?;
+
+    let input_dir: PathBuf = args
+        .next()
+        .expect("Usage: morph <lang> [input_dir] [output_dir]")
+        .into();
+
+    let output_dir: PathBuf = args
+        .next()
+        .expect("Usage: morph <lang> [input_dir] [output_dir]")
+        .into();
+
+    println!("Compiling {} to {:?}", input_dir.to_string_lossy(), lang);
+
+    let mut wrapper = FileWrapper {
+        path: input_dir.clone(),
+        file: File::open(&input_dir)?,
+    };
+
+    let lexer = SimpleLexer;
+    let tokens = lexer.tokenize(&mut wrapper)?;
+
+    let parser = SimpleParser;
+    let packets = parser.parse(&tokens)?;
+
+    println!("\nParsed {} packets:", packets.len());
+    for packet in &packets {
+        println!("  - {} (id: {})", packet.name, packet.id);
     }
 
-}
+    println!("\nGenerating code...");
+    let generator = create_generator(&lang, "me.bottdev.morph".to_string());
+    generator.generate(&output_dir, &packets);
 
-fn print_tokens(tokens: &Vec<Token>) {
-    for token in tokens {
-        println!("{:?}", token);
-    }
+    Ok(())
 }
