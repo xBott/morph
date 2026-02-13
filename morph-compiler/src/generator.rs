99c2a5910cde429968d3fbdf3a4a5958c1089750
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-13 14:32:30 +0100

    moved lang to cli

--- morph-compiler/src/generator.rs
+++ morph-compiler/src/generator.rs
@@ -1,25 +1,6 @@
 use crate::parser::Packet;
 use std::path::Path;
-use std::str::FromStr;
-
-#[derive(Debug)]
-pub enum Lang {
-    Java
-}
-
-impl FromStr for Lang {
-    type Err = std::io::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "java" => Ok(Lang::Java),
-            _ => Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                format!("Unsupported language: {}", s)
-            ))
-        }
-    }
-}
+pub(crate) use crate::cli::Lang;
 
 pub trait Generator {
     fn generate(&self, output_dir: &Path, packets: &Vec<Packet>);
