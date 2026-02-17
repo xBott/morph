c89cb0a0512aa74723d66dbad2221d09349d4c0a
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-17 18:02:16 +0100

    started remaking generator

--- morph-compiler/src/core/generator/generator.rs
+++ morph-compiler/src/core/generator/generator.rs
@@ -2,13 +2,9 @@ use crate::parser::Packet;
 use std::path::Path;
 pub(crate) use crate::cli::Lang;
 
+pub trait GenerationConfig {}
+
 pub trait Generator {
-    fn generate(&self, output_dir: &Path, packets: &Vec<Packet>);
+    fn generate(&self, config: GenerationConfig, packets: &Vec<Packet>);
 
 }
-
-// pub fn create_generator(lang: &Lang, package: String) -> Box<dyn Generator> {
-//     match lang {
-//         Lang::Java => Box::new(crate::generators::JavaGenerator::new(package)),
-//     }
-// }
