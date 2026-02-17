99bb59d01e941ccb3e0e07e4cef3ae812bddb5ef
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-17 13:50:51 +0100

    created semantics module

---
+++ morph-compiler/src/core/semantics/mod.rs
@@ -0,0 +1,9 @@
+mod semantic_analyzer;
+mod analyzers;
+
+pub use semantic_analyzer::*;
+pub use crate::utils::dependency_resolver::*;
+
+pub use analyzers::dependency_semantic_analyzer::*;
+pub use analyzers::composite_semantic_analyzer::*;
+pub use analyzers::name_semantic_analyzer::*;
