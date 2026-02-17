e3df31563cd175fd14a559bef00a5919347cdb20
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-17 18:01:52 +0100

    created id semantic analyzer for analyzing id of packets

--- morph-compiler/src/core/semantics/analyzers/id_semantic_analyzer.rs
+++ morph-compiler/src/core/semantics/analyzers/id_semantic_analyzer.rs
@@ -3,24 +3,39 @@ use crate::core::{Packet, SemanticError};
 use crate::utils::MorphResult::{Errors, Success};
 use crate::utils::{MorphError, MorphResult};
 
-pub struct NameSemanticAnalyzer;
+pub struct IdSemanticAnalyzer;
 
-impl SemanticAnalyzer for NameSemanticAnalyzer {
+impl SemanticAnalyzer for IdSemanticAnalyzer {
 
     fn analyze(&self, packets: &Vec<Packet>) -> MorphResult<()> {
 
         let mut all_errors: Vec<Box<dyn MorphError>> = Vec::new();
-        let mut existing_names: Vec<String> = Vec::new();
+        let mut existing_ids: Vec<i32> = Vec::new();
 
         for packet in packets {
+            
+            if packet.is_auto {
+                continue
+            }
+            
             let packet_name = packet.name.to_string();
-            if existing_names.contains(&packet_name) {
+            let packet_id = packet.id;
+            
+            if packet_id < 0 {
                 let err = SemanticError {
-                    message: format!("Duplicate packet name: {}", packet_name),
+                    message: format!("Id of packet '{}' can not be less than 0, actual value is {}", packet_name, packet_id),
                 };
                 all_errors.push(Box::new(err));
             }
-            existing_names.push(packet_name);
+            
+            if existing_ids.contains(&packet_id) {
+                let err = SemanticError {
+                    message: format!("Duplicate packet id: {}", packet_id),
+                };
+                all_errors.push(Box::new(err));
+            }
+            
+            existing_ids.push(packet_id);
         }
 
         if all_errors.is_empty() {
--- morph-compiler/src/core/semantics/mod.rs
+++ morph-compiler/src/core/semantics/mod.rs
@@ -4,6 +4,8 @@ mod analyzers;
 pub use semantic_analyzer::*;
 pub use crate::utils::dependency_resolver::*;
 
-pub use analyzers::dependency_semantic_analyzer::*;
 pub use analyzers::composite_semantic_analyzer::*;
+pub use analyzers::id_semantic_analyzer::*;
 pub use analyzers::name_semantic_analyzer::*;
+pub use analyzers::dependency_semantic_analyzer::*;
+
