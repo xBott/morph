51dae8875c4cd51b33367784d2e06375743a6229
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-20 14:57:34 +0100

    changed decoder

--- morph-java/morph-java-runtime/src/main/java/me/bottdev/morph/runtime/PacketDecoder.java
+++ morph-java/morph-java-runtime/src/main/java/me/bottdev/morph/runtime/PacketDecoder.java
@@ -1,11 +1,39 @@
 package me.bottdev.morph.runtime;
 
+import me.bottdev.morph.runtime.exceptions.MorphDecodingException;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Optional;
 
 public interface PacketDecoder {
 
-    MorphPacket decode(byte[] data);
+    MorphPacket decodeStream(InputStream in) throws MorphDecodingException;
 
-    Optional<MorphPacket> decodeSafe(byte[] data);
+    default MorphPacket decodeData(byte[] data) {
+        InputStream in = new BufferedInputStream(new ByteArrayInputStream(data));
+        return decodeStream(in);
+    }
+
+    default Optional<MorphPacket> decodeStreamSafe(InputStream in) {
+        try {
+            MorphPacket packet = decodeStream(in);
+            return Optional.of(packet);
+
+        } catch (MorphDecodingException ex) {
+            return Optional.empty();
+        }
+    }
+
+    default Optional<MorphPacket> decodeDataSafe(byte[] data) {
+        try {
+            MorphPacket packet = decodeData(data);
+            return Optional.of(packet);
+
+        } catch (MorphDecodingException ex) {
+            return Optional.empty();
+        }
+    }
 
 }
