8d73f79b3b8be971eee48ac2d7b7d942cb1f979b
_xBott <roma.plakhotniuk07@gmail.com>
2026-02-20 14:58:40 +0100

    encoding using byte array output stream

--- morph-java/morph-java-runtime/src/main/java/me/bottdev/morph/runtime/encoders/SimplePacketEncoder.java
+++ morph-java/morph-java-runtime/src/main/java/me/bottdev/morph/runtime/encoders/SimplePacketEncoder.java
@@ -3,11 +3,15 @@ package me.bottdev.morph.runtime.encoders;
 import me.bottdev.morph.runtime.MorphPacket;
 import me.bottdev.morph.runtime.PacketEncoder;
 
+import java.io.ByteArrayOutputStream;
+
 public class SimplePacketEncoder implements PacketEncoder {
 
     @Override
     public byte[] encode(MorphPacket packet) {
-        return packet.encode();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        packet.encode(out, true);
+        return out.toByteArray();
     }
 
 }
